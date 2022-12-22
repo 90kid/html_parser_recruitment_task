6a628fa0afe3b2adc819e57b8b558ce52fabc73e
90kid <januszakahipis@wp.pl>
2022-12-22 20:05:27 +0100

    delete useless imports

--- src/parseHtmlToCsv.php
+++ src/parseHtmlToCsv.php
@@ -5,11 +5,6 @@ use Dawid\HtmlParser\CsvSaver\CsvSaver;
 use Dawid\HtmlParser\HtmlParser\GivenHtmlPageParser;
 use Dawid\HtmlParser\Models\GivenHtmlPage;
 use PHPHtmlParser\Dom;
-use PHPHtmlParser\Exceptions\ChildNotFoundException;
-use PHPHtmlParser\Exceptions\CircularException;
-use PHPHtmlParser\Exceptions\ContentLengthException;
-use PHPHtmlParser\Exceptions\LogicalException;
-use PHPHtmlParser\Exceptions\StrictException;
 
 
 $filePath = $argv[1] ?? 'example.html';
